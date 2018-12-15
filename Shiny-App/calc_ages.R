914ce8212e0ff181ae8da53e316b415cb89cc0f5
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-14 20:21:48 -0500

    creating the map

--- Shiny-App/calc_ages.R
+++ Shiny-App/calc_ages.R
@@ -2,20 +2,14 @@ library(dplyr)
 library(countrycode)
 library(RColorBrewer)
 
-colnames(min_age_table) <- c("country", "age", "offage")
+colourPalette <- RColorBrewer::brewer.pal(5,"PuRd")
 
-min_age_table$age <- as.numeric(gsub("[*]", "", min_age_table$age))
-
-age_tbl <- min_age_table %>%
-  select(country, age)
-
-age_tbl$code <- countrycode(age_tbl$country, 'country.name', 'iso3c')
+age_tbl <- readRDS("age_tbl.rds")
 
 map <- joinCountryData2Map(age_tbl, joinCode = "ISO3", nameJoinColumn =
                      "code", nameCountryColumn = "country", 
                     suggestForFailedCodes = FALSE, mapResolution = "coarse", 
                     projection = NA, verbose = FALSE)
-
 
 
 mapParams <- mapCountryData(map, nameColumnToPlot = "age", addLegend = FALSE, 
@@ -24,15 +18,4 @@ mapParams <- mapCountryData(map, nameColumnToPlot = "age", addLegend = FALSE,
                             numCats = length(unique(map$age)), colourPalette = colourPalette, 
                             mapTitle = "", borderCol = "black")
 
-colourPalette <- RColorBrewer::brewer.pal(5,"PuRd")
-
-
-
-do.call( addMapLegend, c(mapParams, legendLabels= "all", legendWidth=0.5, legendMar = 2))
-
-
-data(age_tbl)
-
-saveRDS(age_tbl, "age_tbl.rds")
-
-colourPalette <- brewer.pal(age_tbl$age, 'RdYlGn')
+do.call(addMapLegend, c(mapParams, legendLabels= "all", legendWidth=0.5, legendMar = 2))
