46249d576d1a7b493efd48ffac71f7f6383c3feb
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-14 21:09:49 -0500

    adding comments

--- Shiny-App/beer data/03_trim_city_whitespace.R
+++ Shiny-App/beer data/03_trim_city_whitespace.R
@@ -1,7 +1,17 @@
 library(dplyr)
 
+# From the way the data was organized, some whitespace remains surrounding the 
+# city names, which negatively impacts sorting functions. 
+
+# Although this could be completed in 02 file before saving the loc_all.rds,
+# it would require another lengthy pull from the geocodes API to accomplish. 
+# Therefore, we remove whitespace here, separately. 
+
+# Read in the data frame
 data_now <- readRDS("beer_data_loc_all.rds")
 
+# Trim any whitespace surrounding the city name
 data_now$city <- trimws(as.character(data_now$city))
 
+# Save the new data frame
 saveRDS(data_now, "beer_data_loc_all.rds")
